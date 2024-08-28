19955b2fbff57719e3237038f167d0496713be37
AvigailSalomons <a0548543860@gmail.com>
2024-08-28 13:49:27 +0300

    befor change the pages

--- src/images.jsx
+++ src/images.jsx
@@ -2,52 +2,64 @@ import React, { useState } from 'react';
 import { MDBCarousel, MDBCarouselItem, MDBCarouselCaption } from 'mdb-react-ui-kit';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './styles.scss';
-import './images.css';
-import img1 from './images/1.jpeg';
-import img2 from './images/2.jpeg';
-import img3 from './images/3.jpeg';
-import img4 from './images/4a.jpeg';
+// import img1 from './images/11.jpg';
+// import img3 from './images/3.jpeg';
+// import img4 from './images/4a.jpeg';
+// import img5 from './images/8.jpeg';
+import img6 from './images/16.jpg';
+import img7 from './images/15.JPG';
+import img8 from './images/17.JPG';
+import img9 from './images/18.JPG';
+import img10 from './images/19.JPG';
+import img11 from './images/20.JPG';
+import img12 from './images/21.JPG';
+import img13 from './images/22.JPG';
+import img14 from './images/23.JPG';
+import img615 from './images/24.JPG';
+import img16 from './images/25.JPG';
+import img17 from './images/26.JPG';
+import img18 from './images/27.JPG';
+import img19 from './images/28.JPG';
+import img20 from './images/29.JPG';
+import img21 from './images/30.JPG';
+import img22 from './images/31.JPG';
+import img23 from './images/32.JPG';
+
+
+
+import img24 from './images/6.jpeg';
+
+
+
 import CarouselControlsReplacement from './imageButton.jsx';
 
-const images = [img3, img1, img2, img4]; // Array of images
+const images = [img7,img6,img8,img9,img10,img11,img12,img13,img14,img615,img16,img17,img18,img19,img20,img21,img22,img23,img24]; 
 
 export default function Images() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handleThumbnailClick = (index) => {
     setActiveIndex(index); // Update active index to the clicked thumbnail index
+    console.log(index, "index");
   };
 
   return (
-    
     <div id="carousel-all" className="carousel-container">
-      
-      <div className="thumbnail-column">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            className='thumbnail'
-            alt={`Thumbnail ${index + 1}`}
-            onClick={() => handleThumbnailClick(index)} // Set active index on click
-          />
-        ))}
-      </div>
-    
-      <MDBCarousel activeItem={activeIndex + 1} showIndicators showControls fade>
+      <MDBCarousel showIndicators showControls fade activeItem={activeIndex + 1} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         {images.map((image, index) => (
           <MDBCarouselItem itemId={index + 1} key={index}>
-            <img src={image} className='d-block w-100 img' alt={`Slide ${index + 1}`} />
+            <div id="img-div">
+            <img src={image} className="img" alt={`Slide ${index + 1}`} />
+            </div>
             <MDBCarouselCaption>
-              <h5>Slide label {index + 1}</h5>
-              <p>Slide description for image {index + 1}</p>
+              {/* <h5>Slide label {index + 1}</h5> */}
+          
             </MDBCarouselCaption>
           </MDBCarouselItem>
         ))}
       </MDBCarousel>
 
       <CarouselControlsReplacement id="CarouselControlsReplacement" />
-  
-  </div>
+    </div>
   );
 }
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,69 +1,60 @@
 import React from 'react';
 import './navbar.css';
+import persons from './images/persons.png';
+import gallry from './images/interface_13492152.png';
+import location from './images/location_535239.png';
+import contact from './images/contact-mail_3095583.png';
+import calendar from './images/calendar_2693507.png';
+
+
+
 
 function NavBar() { 
     return (
         <div>
             <nav id="mainNav" className="hidden-xs navbar-default navbar-fixed-top opacity-full escm-observing">
                 <div className="site_container">
-                    <div className="navbar-header">
-                        <a href="#page-top" className="s123-site-logo navbar-brand s123-w-l-s page-scroll">
-                            <span className="website-name website-name-preview-helper">סוויטת היהלום שבחושן</span>
-                        </a>
-                    </div>
+                  
                     <div id="top-menu">
                         <ul className="navPages nav navbar-nav">
                             <li className="moduleMenu" data-menu-module-id="65083ab45ac91">
                                 <a className="page-scroll" href="#section-65083ab45ac91">
                                     <span className="txt-container"> מה בסביבה</span>
+                                    <img src={persons}   className="image-icone" />
+                                </a>
+                            </li>
+                             
+                            <li id="centerLogo19">
+                                <a href="javascript:void(0);" className="s123-site-logo navbar-brand s123-w-l-s page-scroll">
+                                    <span className="txt-container">מיקום</span>
+                                    <img src={location}   className="image-icone" />
                                 </a>
                             </li>
                             <li className="moduleMenu" data-menu-module-id="65083ab45ac91">
                                 <a className="page-scroll" href="#section-65083ab45ac91">
                                     <span className="txt-container"> לבדיקת זמינות</span>
+                                    <img src={calendar}   className="image-icone" />
                                 </a>
                             </li>
                             <li className="moduleMenu" data-menu-module-id="64f0607fe361d">
                                 <a className="page-scroll" href="#section-64f0607fe361d">
                                     <span className="txt-container">גלריה</span>
+                                 <img src={gallry}   className="image-icone" />
                                 </a>
                             </li>
                             <li id="centerLogo19">
                                 <a href="javascript:void(0);" className="s123-site-logo navbar-brand s123-w-l-s page-scroll">
-                                    <span className="website-name website-name-preview-helper">להזמנה ויצירת קשר</span>
+                                    <span className="txt-container">להזמנה ויצירת קשר</span>
+                                    <img src={contact}   className="image-icone" />
                                 </a>
                             </li>
-                            <li className="moduleMenu extra-nav-more dropdown-submenu">
-                                <ul className="site-dropdown-menu">
-                                    <li className="moduleMenu" data-menu-module-id="6508471856e96">
-                                        <a className="page-scroll" href="#section-6508471856e96">
-                                            <span className="txt-container">גלריית תמונות</span>
-                                        </a>
-                                    </li>
-                                    <li className="moduleMenu" data-menu-module-id="5b0a4ba521825">
-                                        <a className="page-scroll" href="#section-5b0a4ba521825">
-                                            <span className="txt-container">הזמינו עכשיו</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
+                          
                         </ul>
                         <ul className="navActions nav navbar-nav">
-                            <li className="website-languages-menu">
-                                <a className="btn website-languages-menu-link"></a>
-                            </li>
-                            <li className="header-social-wrapper header-icon-wrapper">
-                                <a data-close-location="right" className="actionButton" role="button" data-container="body" data-toggle="social_menuCallActionIcons">
-                                    <i className="svg-m s123-icon-converter" alt="share-alt">&nbsp;</i>
-                                </a>
-                            </li>
-                            <li className="header-cart-wrapper orderOpenCart" data-has-online-store="false">
-                                <a className="actionButton btn-primary-action-button-4" role="button">
-                                    <i className="svg-m s123-icon-converter" alt="shopping-cart">&nbsp;</i>
-                                    <span className="count"></span>
-                                </a>
-                            </li>
                         </ul>
+                    </div>
+                    <div className="navbar-header">
+                            <span className="website-name">אחוזת היהלום שבחושן</span>
                     </div>
                 </div>
             </nav>
